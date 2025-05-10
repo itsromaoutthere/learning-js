920fc4f63116e4774489341f92fa1d8fdd1d057c
Roma <romaa29032006@gmail.com>
2025-05-10 19:56:56 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score=33
+console.log(score)
+
+let num=Number(score)
+console.log(num)
+console.log(typeof num);
+
+let ispossible=true
+console.log(ispossible)
+let booleannum=Number(ispossible)
+console.log(booleannum)
+
+let a_num="33ab"
+let new_num=Number(a_num)
+console.log(typeof new_num)
+console.log(new_num)
+
+//"33"=.33
+//"33ab"=NaN
+// True=> 1 ; false=> 0
+
+let isloggedIn="name"
+let thisisloggedIn=Boolean(isloggedIn)
+console.log(thisisloggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"name" => true
+
+let someNum = 33
+let stringNumber= String(someNum)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//33 => "33" //converting it into strings
