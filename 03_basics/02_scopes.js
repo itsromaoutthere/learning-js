787f08aef9e75a0eca81139fbb213c512900e897
Roma <romaa29032006@gmail.com>
2025-05-14 16:13:20 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,16 @@
-//Scopes
+//Scopes: Block, Function and Global
 
+//var keyword cannot have block scope
 let a=10
 const b=20
+
+if(true){
+    let a=19
+
+    let c=30 //
+    console.log("inner:",a);
+}
+
+console.log(a);
+console.log (b);
+
