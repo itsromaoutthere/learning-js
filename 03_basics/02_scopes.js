43b2cf182e9d0f6521326b3bea202ec4f52f5cef
Roma <romaa29032006@gmail.com>
2025-05-14 18:21:28 +0000

    Nested scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -14,3 +14,43 @@ if(true){
 console.log(a);
 console.log (b);
 
+//Nested scopes
+
+function one(){
+    const username = "Suraj"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    //console.log(website); //ERROR
+
+    two()
+}
+
+one()
+
+if (true){
+    const username = "hello"
+    if (username === "hello"){
+        const website="youtube"
+        console.log(username+website);
+    }
+    //console.log(website);//ERROR
+
+}
+
+//console.log(username);//ERROR
+
+//suppose
+function addOne(num){
+    return num + 1
+}
+console.log(addOne(5));
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
+
