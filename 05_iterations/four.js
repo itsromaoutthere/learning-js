774ae2d8291e8f25f04004854eb08a3977ec4639
Roma <romaa29032006@gmail.com>
2025-05-18 11:25:02 +0000

    ForIn

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+//object:it is iterable
+const myObject = {
+    js: 'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for(const key in myObject){
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming=["js","rb","py","java"]
+for (const key in programming) {
+   console.log(programming[key]);
+}
